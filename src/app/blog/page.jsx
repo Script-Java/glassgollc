a08b683c7e5f621c5e37cdf360173417cecb2777
Script-Java <atrindev@gmail.com>
2025-06-17 08:20:30 -0500

    u[date

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import { getSortedPostsData } from "@/lib/markdown";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
 
 export default function Blog() {
     const posts = getSortedPostsData();
