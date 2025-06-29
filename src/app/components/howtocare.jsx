8d840f5c2b52359729be09a0f4bca916d21c8b00
Script-Java <atrindev@gmail.com>
2025-06-29 15:39:45 -0500

    added

--- src/app/components/howtocare.jsx
+++ src/app/components/howtocare.jsx
@@ -1,13 +1,13 @@
+import { FaCarSide, FaHome, FaTools, FaCheckCircle } from 'react-icons/fa';
 import Link from "next/link";
 
 export default function HowToCare() {
   return (
-    <section className="bg-base-100 py-10 px-6">
-      {/* Bottom CTA Button */}
+    <section className="bg-base-100 py-10 px-4 md:px-6">
       <div className="mt-4 flex justify-center">
         <Link
           href="/blog/how-to"
-          className="btn bg-blue-900 text-white hover:bg-primary text-lg px-6 py-3 rounded-md shadow-md text-center max-w-full"
+          className="btn bg-blue-900 text-white hover:bg-primary text-center px-4 py-3 w-full max-w-2xl text-sm sm:text-base md:text-lg lg:text-xl"
         >
           HOW TO CARE FOR A NEWLY REPLACED WINDSHIELD AND BACK GLASS
         </Link>
