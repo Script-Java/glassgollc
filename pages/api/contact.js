8e8c356d687d9ec81cc2dc3aa53f40d272b520b0
Script-Java <atrindev@gmail.com>
2025-07-23 23:08:39 -0500

    update

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -110,7 +110,7 @@ export default async function handler(req, res) {
       };
 
       await transporter.sendMail(mailOptions);
-      res.status(200).json({ message: "Message sent successfully!" });
+      res.status(200).json({ message: "Message sent successfully! Thank you, We'll be in touch soon!" });
     } catch (error) {
       console.error("Error sending email:", error);
       res.status(500).json({ error: "Failed to send message." });
